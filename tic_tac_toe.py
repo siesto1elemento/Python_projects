45aaa685a2806f2edbaa6587a417bd8b4b53c995
Rohit ojha <89785142+siesto1elemento@users.noreply.github.com>
2023-04-01 16:55:06 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,11 +3,20 @@ game = [[0, 0, 0],
         [0, 0, 0],]
 
 
-print("   a  b  c")
 
+def game_board(player=0, row=0, column=0, just_display= False):
+        print("   a  b  c")
 
-for count,row in enumerate (game):
-    print(count, row)
+        if not just_display:
+                game[row][column] = player
+        for count,row in enumerate (game):
+                print(count, row)
+game_board(just_display=True)
+game_board(player=1,row = 2, column = 0)
+#game[0][1] = 1
+    
+    
+
     
     
 
