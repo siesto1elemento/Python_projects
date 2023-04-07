99caf924c273b321117c081313ceddff7bdc8628
siesto1elemento <rohitojha9720@gmail.com>
2023-04-07 22:19:16 +0530

    Made changes for the dealer cards

--- blackjack.py
+++ blackjack.py
@@ -7,7 +7,7 @@ J = 10
 K = 10
 Q = 10
 
-def hit(my_cards, dec):
+def hit(my_cards, deck):
     random_card = random.choice(deck)
     my_cards.append(random_card)
     return my_cards
@@ -19,16 +19,23 @@ deck = [2,3,4,5,6,7,8,9,10,A,J,K,Q,2,3,4,5,6,7,8,9,10,A,J,K,Q,2,3,4,5,6,7,8,9,10
 my_cards = random.sample(deck, 2)
 dealer_cards = random.sample(deck, 2)
 
-for i in dealer_cards:
-    if ((dealer_cards[i] + dealer_cards[i + 1]) < 17): # if the first and second card do not add upto 17, we will append another card,
-        random_card = random.choice(deck)              # we then check if all three of them add upto 17 or more and then append another if they dont
-        dealer_cards.append(random_card)
+while sum(dealer_cards) < 17:# until the sum of the dealer cards is less than 17
+    random_card = random.choice(deck)# we will keep appending cards to the list
+    dealer_cards.append(random_card)
 
 print(my_cards)
-print(dealer_cards) # only print the first card as this is the dealer
+print(dealer_cards[0]) # only print the first card as this is the dealer
 
 if input() == 'hit':
     print(hit(my_cards, deck))
+
+print(dealer_cards) # we finally print the dealer cards to see we win or not
+
+if (sum(my_cards) > sum(dealer_cards)) and sum(my_cards) >= 21:
+    print('Dealer wins')
+
+else:
+    print('You Win, congratulation')
 
 
 
